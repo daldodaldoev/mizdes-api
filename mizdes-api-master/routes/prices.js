c76a51f6cb82c56f509ff9188a017f039c15a4a8
ss <maximmm7771@mail.ru>
2024-04-19 18:04:53 +0300

    first commit

--- mizdes-api-master/routes/prices.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-const { getLatestPrices } = require("../controllers/prices");
-
-router.get("/api/prices", getLatestPrices);
-
-module.exports = router;
